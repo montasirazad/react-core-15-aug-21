e9662a330f8f10f2ea939b15268504b579d3b27d
unknown <montasirazad@gmail.com>
2021-08-21 02:53:25 +0600

    practice

--- src/App.js
+++ src/App.js
@@ -6,20 +6,28 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React core </h1>
+        <Name name='S.M'/>
+        <Name name='Dhrubo'/>
+        <Name name='montasir'/>
+         
+        
       </header>
     </div>
   );
+function Name(props) {
+  const nameStyle = {
+
+  }
+ return (
+  <div style ={{border:'5px dotted yellow',
+  padding:'10px',margin:'8px'}}>
+  {/* <h1>Name: AZAD</h1> */}
+  <h2>Name:{props.name}</h2>
+</div> 
+ )
+}
+
 }
 
 export default App;
